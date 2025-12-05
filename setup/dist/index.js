b0579370479c5cef9c712d19414a955281dd8b3a
Prasanna Achar <acharprasanna5@gmail.com>
2025-12-05 21:55:21 +0530

    ok

---
+++ setup/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}`;
+}
+const name = "Raju";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
---
+++ setup/src/index.ts
@@ -0,0 +1,7 @@
+function greet(name : string): string{
+    return `Hello, ${name}`;
+}
+
+const name : string= "Raju"
+
+console.log(greet(name))
